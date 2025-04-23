75c9ffde2cae97651f87d68d35ee251142bddb55
Jonathan <jonathan.strickland2012@gmail.com>
2025-04-22 21:49:29 -0230

    update images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="home-container">
         <div className="profile-card">
           <img
-            src="/portfolio/images/headshot.jpg"
+            src="/portfolio/public/images/headshot.jpg"
             alt="Your headshot"
             className="profile-img"
           />
