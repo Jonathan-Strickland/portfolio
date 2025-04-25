a75f9a37119611c87d58184150c5e6ef3492d7a1
Jonathan <jonathan.strickland2012@gmail.com>
2025-04-25 18:08:38 -0230

    updated css for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,25 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
-        <h1 className="nav-name">Jonathan Strickland</h1>
-      <ul className="nav-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/projects">Projects</Link></li>
-        <li><Link to="/badges">Badges</Link></li>
-        <li><Link to="/sudokumenu">Sudoku</Link></li>
-        <li><Link to="/dodgeenemy">Dodge the Enemy</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+      <h1 className="nav-name">Jonathan Strickland</h1>
+
+      <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+        &#9776;
+      </div>
+
+      <ul className={`nav-links ${menuOpen ? "open" : ""}`}>
+        <li><Link to="/" onClick={() => setMenuOpen(false)}>Home</Link></li>
+        <li><Link to="/projects" onClick={() => setMenuOpen(false)}>Projects</Link></li>
+        <li><Link to="/badges" onClick={() => setMenuOpen(false)}>Badges</Link></li>
+        <li><Link to="/sudokumenu" onClick={() => setMenuOpen(false)}>Sudoku</Link></li>
+        <li><Link to="/dodgeenemy" onClick={() => setMenuOpen(false)}>Dodge the Enemy</Link></li>
+        <li><Link to="/contact" onClick={() => setMenuOpen(false)}>Contact</Link></li>
       </ul>
     </nav>
   );
